989076044dbb96085b3bdfbe3e9d51dd19b4a8b3
Adrien Le Guillou <git@aleguillou.org>
2024-05-06 13:06:05 +0200

    up

--- R/z-test.R
+++ R/z-test.R
@@ -1,8 +1,8 @@
 # Scratchpad for interactive testing before integration in a script
 
-rgc.prob,0.2662722,numeric, CPN-plogis(qlogis(0.25546) + log(1.25)) #0.357;  # gaps appendix 9.4
-ugc.prob,0.225,numeric,CPN-gaps appendix 9.4 | calibrated
-rct.prob,0.1923077,numeric, CPN-plogis(qlogis(1714429) + log(1.25)) #0.3216; # gaps appendix 9.3
-uct.prob,0.16,numeric, CPN-gaps appendix 9.3 | calibrated
+# rgc.prob,0.2662722,numeric, CPN-plogis(qlogis(0.25546) + log(1.25)) #0.357;  # gaps appendix 9.4
+# ugc.prob,0.225,numeric,CPN-gaps appendix 9.4 | calibrated
+# rct.prob,0.1923077,numeric, CPN-plogis(qlogis(1714429) + log(1.25)) #0.3216; # gaps appendix 9.3
+# uct.prob,0.16,numeric, CPN-gaps appendix 9.3 | calibrated
 
 plogis(qlogis(0.16) + log(1.25))
