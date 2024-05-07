04b00b48c38c11237dba72cf98467b40832122f9
UddieEpi <uonwubi@emory.edu>
2024-05-07 13:19:07 -0400

    Create 42.3-removefiles_mddtbl2.R

---
+++ R/C-netsim_scenarios/42.3-removefiles_mddtbl2.R
@@ -0,0 +1,18 @@
+##
+## Remove sim and log files after processing for table 2 simulations
+##
+
+#remove sim files
+sims_dir <- paste0("data/intermediate/scenarios_tbl2")
+
+files_to_delete_1 <- dir(path = sims_dir, pattern = "^sim__.*rds$")
+file.remove(file.path(sims_dir, files_to_delete_1))
+
+
+
+
+#remove log files
+log_dir <- paste0("workflows/mddtbl2/log")
+
+files_to_delete_2 <- dir(path = log_dir, pattern = "^mddtbl2_step.*out$")
+file.remove(file.path(log_dir, files_to_delete_2))
