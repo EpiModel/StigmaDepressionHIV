9b460f99600daef958dbb2d95e5fdbfe00149c49
UddieEpi <uonwubi@emory.edu>
2024-07-05 12:53:25 -0400

    Update shared_variables.R

--- R/shared_variables.R
+++ R/shared_variables.R
@@ -10,10 +10,10 @@ EMHIVp_dir    <- "../EpiModelHIV-p.git/StigmaDepressionHIV_real"
 time_unit  <- 7               # number of days in a time step
 year_steps <- 364 / time_unit # number of time steps in a year
 
-calibration_end    <- 65 * year_steps
+calibration_end    <- 70 * year_steps  #note: 65 years (calib) used for defense model run
 restart_time       <- calibration_end + 1
 prep_start         <- 0
-intervention_start <- restart_time + 10 * year_steps
+intervention_start <- restart_time + 5 * year_steps  #note: plus 10*year_steps used for defense model run
 intervention_end   <- intervention_start + 10 * year_steps
 
 
