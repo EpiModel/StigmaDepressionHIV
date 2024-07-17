70f18eac586b239800ebe74b6430c18113adb4e7
UddieEpi <uonwubi@emory.edu>
2024-07-17 08:10:49 -0400

    Update 52.4-removefiles_AllSims_mddtbl2.R

--- R/C-netsim_scenarios/52.4-removefiles_AllSims_mddtbl2.R
+++ R/C-netsim_scenarios/52.4-removefiles_AllSims_mddtbl2.R
@@ -3,9 +3,9 @@
 ##
 
 #remove all sim files
-sims_dir <- paste0("data/intermediate/scenarios_mddtbl2")
+sims_dir <- paste0("data/intermediate/scenarios_mddtbl2/redusims")
 
-files_to_delete_1 <- dir(path = sims_dir, pattern = "^sim__.*rds$")
+files_to_delete_1 <- dir(path = sims_dir, pattern = "^df__.*rds$")
 file.remove(file.path(sims_dir, files_to_delete_1))
 
 
