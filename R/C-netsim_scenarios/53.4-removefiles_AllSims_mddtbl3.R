9a94ea6cfa4ebf1af1c8a8b349a875ffc9111ce2
UddieEpi <uonwubi@emory.edu>
2024-07-31 18:34:58 -0400

    Create 53.4-removefiles_AllSims_mddtbl3.R

---
+++ R/C-netsim_scenarios/53.4-removefiles_AllSims_mddtbl3.R
@@ -0,0 +1,12 @@
+##
+## Remove sim and log files after processing for table 3 simulations
+##
+
+#remove all sim files
+sims_dir <- paste0("data/intermediate/scenarios_mddtbl3/redusims")
+
+files_to_delete_1 <- dir(path = sims_dir, pattern = "^df__.*rds$")
+file.remove(file.path(sims_dir, files_to_delete_1))
+
+
+
